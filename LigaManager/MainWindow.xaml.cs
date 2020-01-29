94583c63a44094baf53b59385814af8ac48fd436
Cristhian <cristhianbay@gmail.com>
2020-01-29 18:41:42 +0100

    Implementado código que rellena el DataGrid

--- LigaManager/MainWindow.xaml.cs
+++ LigaManager/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +25,26 @@ namespace LigaManager
         public MainWindow()
         {
             InitializeComponent();
+    
+        }
+
+        private void onClickSalir(object sender, RoutedEventArgs e)
+        {
+            /** Cerrará la aplicación completamente */
+            Application.Current.Shutdown();
+        }
+
+        void InitializeDataGrid()
+        {
+            MySqlConnection connection =
+                new MySqlConnection(@"server=localhost;userid=dbuser;password=s$cret;database=testdb");
+         
+            connection.Open();
+
+            MySqlCommand command = new MySqlCommand("SELECT * FROM jugador", connection);
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+            DataTable dataTable = new DataTable("Jugadores");
+            adapter.Fill(dataTable);
         }
     }
 }
