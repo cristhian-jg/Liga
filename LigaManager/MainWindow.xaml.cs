a012ec76a9a77904e36de1db80a3bec71072aa6c
Cristhian <cristhianbay@gmail.com>
2020-02-04 20:48:04 +0100

    Removidos imports innecesarios

--- LigaManager/MainWindow.xaml.cs
+++ LigaManager/MainWindow.xaml.cs
@@ -1,19 +1,8 @@
 ﻿using MySql.Data.MySqlClient;
 using System;
-using System.Collections.Generic;
 using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace LigaManager
 {
@@ -112,7 +101,7 @@ namespace LigaManager
             else
             {
                 MessageBox.Show("Selecciona a un jugador para modificarlo.", "Operación no permitida",
-   MessageBoxButton.OK, MessageBoxImage.Warning);
+                    MessageBoxButton.OK, MessageBoxImage.Warning);
             }
 
         }
